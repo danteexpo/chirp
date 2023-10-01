df3a09de678843579801e3fe55e756050e630bc6
Dante <dante0im@gmail.com>
2023-10-01 14:57:10 -0300

    Router created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data } = api.posts.getAll.useQuery();
 
   const { isSignedIn } = useUser();
 
@@ -17,8 +17,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {!!isSignedIn && <SignOutButton />}
-        {!isSignedIn && <SignInButton />}
+        <div>
+          {!!isSignedIn && <SignOutButton />}
+          {!isSignedIn && <SignInButton />}
+        </div>
+        <div>
+          {data?.map((post) => <div key={post.id}>{post.content}</div>)}
+        </div>
       </main>
     </>
   );
