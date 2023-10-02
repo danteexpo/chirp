e170af62d6e35f8889018787c8717c0bf6123d83
Dante <dante0im@gmail.com>
2023-10-02 00:28:53 -0300

    trpc helpers for profile page & created Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,16 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Chirp | Post Emojis!</title>
+        <meta name="description" content="Chirp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
